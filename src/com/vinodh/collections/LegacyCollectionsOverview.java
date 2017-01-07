7bd6dcaf364cdbcbffd71a811a813b7bb44f001b
vinodh kumar Thimmisetty <vinodh5052@gmail.com>
2017-01-08 00:10:40 +0530

    Create LegacyCollectionsOverview.java

---
+++ src/com/vinodh/collections/LegacyCollectionsOverview.java
@@ -0,0 +1,61 @@
+package com.vinodh.collections;
+
+import java.util.Vector;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class LegacyCollectionsOverview {
+
+
+/**
+*java.lang.Object
+* java.util.AbstractCollection<E>
+*   java.util.AbstractList<E>
+*     java.util.Vector<E>
+*
+ * All Implemented Interfaces: Serializable, Cloneable, Iterable<E>, Collection<E>, List<E>, RandomAccess
+ * Direct Known Subclasses: Stack
+ * 
+ * Data Structure : Vector --> Resizble or Growable ARRAY implemention
+ *                  (Default capacity is 10 & it will double and copy the existing to new if vetor capacity > 10)
+ * Insertion Order : Preserved
+ * Duplicates : Allowed
+ * Allows any type of objects [Homogenous or Heterogenous]
+ * NULL insertions are possible
+ * Synchoronized : YESSSSSSSS !!
+ * Support Generics, ENUMERATION
+ * Support Iterators & forEach & Streams(java 8) 
+ * 		[[ elements in the list can be added/removed with help of Iterators but not with forEach ]] 	
+ * Algorithm Features:
+ *  Searching: Easy based on Index(Random access)
+ *  Deletion : @ end is easy
+ *  Insertion : @ end is easy
+ *  Sorting : Can be sort with help of Collections utility class
+ * @author Vinodh Kumar Thimmisetty
+ *
+ */
+class VectorOverview{
+
+
+}
+/**
+*java.lang.Object
+*  java.util.AbstractCollection<E>
+*     java.util.AbstractList<E>
+*       java.util.Vector<E>
+*           java.util.Stack<E>
+*   All Implemented Interfaces: Serializable, Cloneable, Iterable<E>, Collection<E>, List<E>, RandomAccess
+*
+*
+*/
+class StackOverview{
+
+}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) { 
+  
+	}
+}
